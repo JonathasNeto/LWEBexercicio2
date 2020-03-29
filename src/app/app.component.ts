e04e08851c4ac6f4448037c7984320170fbf9cf4
unknown <jonetocrazy@gmail.com>
2020-03-29 17:11:40 -0300

    exercicio2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Projeto';
+  
+
+  nome = null;
+  
+  pessoas= [
+    
+  ];
+  adicionar(){
+    
+    this.pessoas.push(new Pessoa(this.nome));
+    this.nome = null;
+    
+   
+  }
+  excluir(id){
+    this.pessoas.splice(this.pessoas.indexOf(id),1); 
+  }
+
+
+}
+class Pessoa{
+  nome = null;
+  
+  constructor(nome){
+    this.nome = nome;
+    
+  }
+  
+  toString(){
+    return this.nome;
+  }
 }
